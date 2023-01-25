2113bbbf9f6882e379ea70e7c4fe9a201840ad39
AkshDesai04 <dev.akshdesai@gmail.com>
2023-01-25 08:07:13 +0530

    Added board Initialisation

---
+++ Initialise.java
@@ -0,0 +1,22 @@
+public class Initialise {
+	static void initialiseBoard(String[][] board) {
+		for(int i = 0;i < 8;i++)
+			for(int j = 0;j < 8;j++)
+				board[i][j] = "-";
+
+		for(int i = 0;i < 8;i++)    board[6][i] = "WP";     //White Pawns
+		for(int i = 0;i < 8;i++)    board[1][i] = "BP";     //Black Pawns
+
+		board[0][0] = "BR";         board[0][7] = "BR";     //Black Rook
+		board[7][0] = "WR";         board[7][7] = "WR";     //White Rook
+
+		board[0][1] = "BH";         board[0][6] = "BH";     //Black Knight
+		board[7][1] = "WH";         board[7][6] = "WH";     //White Knight
+
+		board[0][2] = "BB";         board[0][5] = "BB";     //Black Bishop
+		board[7][2] = "WB";         board[7][5] = "WB";     //White Bishop
+
+		board[0][3] = "BQ";         board[0][4] = "BK";     //Black Queen and King
+		board[7][3] = "WQ";         board[7][4] = "WK";     //White Queen and King
+	}
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static String[][] board;
+	public static void main(String[] args) {
+		board = new String[8][8];
+		Initialise.initialiseBoard(board);
+	}
+}
