4724c45611326f9393ec2e791555c8584fa57668
AkshDesai04 <dev.akshdesai@gmail.com>
2023-01-26 18:14:02 +0530

    Added moves for the king

--- Chess/Pieces/King.java
+++ Chess/Pieces/King.java
@@ -3,6 +3,7 @@ package Chess.Pieces;
 import javax.swing.*;
 
 public class King extends Piece {
+	boolean castle_rights = true;
 	public King(int color, int[] position) {
 		name = (color == 1 ? "White":"Black") + " King";
 		code = (color == 1 ? "W":"B") + "K";
@@ -13,6 +14,16 @@ public class King extends Piece {
 	public boolean[][] possibleMoves(Piece[][] board, int[] position) {
 		boolean[][] moves = new boolean[8][8];
 		for(int i = 0;i < 8;i++)    for(int j = 0;j < 8;j++)    moves[i][j] = false;
+
+		try{moves[position[0] + 1][position[1] + 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0] + 1][position[1] - 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0] - 1][position[1] + 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0] - 1][position[1] - 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0] + 1][position[1]] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0] - 1][position[1]] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0]][position[1] + 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+		try{moves[position[0]][position[1] - 1] = true;}    catch(ArrayIndexOutOfBoundsException ignored) {}
+
 		return moves;
 	}
 }
