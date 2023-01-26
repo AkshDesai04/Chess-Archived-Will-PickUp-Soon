490c59e5fc0ed1ac27368f53aebfede655cbac34
AkshDesai04 <dev.akshdesai@gmail.com>
2023-01-26 19:45:06 +0530

    Added Rook moves

--- Chess/Pieces/Rook.java
+++ Chess/Pieces/Rook.java
@@ -15,6 +15,20 @@ public class Rook extends Piece {
 	public boolean[][] possibleMoves(Piece[][] board, int[] position) {
 		boolean[][] moves = new boolean[8][8];
 		for(int i = 0;i < 8;i++)    for(int j = 0;j < 8;j++)    moves[i][j] = false;
+
+		for(int i = 1;i < 8;i++)        //Up
+			try {moves[position[0] - i][position[1]] = true;}
+			catch (ArrayIndexOutOfBoundsException ignored) {}
+		for(int i = 1;i < 8;i++)        //Down
+			try {moves[position[0] + i][position[1]] = true;}
+			catch (ArrayIndexOutOfBoundsException ignored) {}
+		for(int i = 1;i < 8;i++)        //Left
+			try {moves[position[0]][position[1] - i] = true;}
+			catch (ArrayIndexOutOfBoundsException ignored) {}
+		for(int i = 1;i < 8;i++)        //Right
+			try {moves[position[0]][position[1] + i] = true;}
+			catch (ArrayIndexOutOfBoundsException ignored) {}
+
 		return moves;
 	}
 }
